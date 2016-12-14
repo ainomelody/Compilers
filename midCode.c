51e0dd9fea2fef3b4cdc3c7055807a55e4e0cac5
ainomelody <qingqingdao5566@126.com>
2016-12-14 21:23:32 +0800

    some errors.add function

--- midCode.c
+++ midCode.c
@@ -1,26 +1,33 @@
 #include "midCode.h"
+#include <stdlib.h>
+
+#define TEMPVARNUM 10
 
 static codeCollection allCodes;
 static funcCode *curFunc;
+int labelNum;
+static int tempVarIndex;
+static int tempVarUsed[TEMPVARNUM];
 
 void initCodeCollection()
 {
-    allCodes->data = malloc(sizeof(funcCode *) * LISTSIZE);
-    allCodes->size = LISTSIZE;
-    allCodes->pos = 0;
+    allCodes.data = malloc(sizeof(funcCode *) * LISTSIZE);
+    allCodes.size = LISTSIZE;
+    allCodes.pos = 0;
 }
 
 void addFunction(funcInfo *func)
 {
-    if (allCodes->size == allCodes->pos) {
-        allCodes->size += LISTINCSIZE;
-        allCodes->data = relloc(allCodes->data, sizeof(funcCode *) * allCodes->size);
+    if (allCodes.size == allCodes.pos) {
+        allCodes.size += LISTINCSIZE;
+        allCodes.data = realloc(allCodes.data, sizeof(funcCode *) * allCodes.size);
     }
     curFunc = malloc(sizeof(funcCode));
-    allCodes->data[allCodes->pos++] = curFunc;
+    allCodes.data[allCodes.pos++] = curFunc;
     curFunc->space = 0;
     curFunc->code = NULL;
     curFunc->info = func;
+    curFunc->toAlloc = newVarList();
 }
 
 void addLocalVar(varInfo *var)
@@ -48,4 +55,65 @@ void addCode(int op, int target, int arg1, int arg2)
         newCode->next = curFunc->code;
         curFunc->code->prev = newCode;
     }
+}
+
+void modifyPrevTarget(int target)
+{
+    curFunc->code->prev->target = target;
+}
+
+int getTempVar()
+{
+    while (tempVarUsed[tempVarIndex])
+        tempVarIndex = (tempVarIndex + 1) % TEMPVARNUM;
+    tempVarUsed[tempVarIndex] = 1;
+    return tempVarIndex;
+}
+
+void releaseTempVar(int index)
+{
+    tempVarUsed[index] = 0;
+}
+
+expTransInfo translateExp(Node *node)
+{
+
+}
+
+void addIOFunc()
+{
+    funcInfo *readInfo, *writeInfo;
+    symNode *symRead, *symWrite, *insLoc;
+    varInfo *param;
+
+    readInfo = malloc(sizeof(funcInfo));
+    writeInfo = malloc(sizeof(funcInfo));
+    symRead = malloc(sizeof(symNode));
+    symWrite = malloc(sizeof(symNode));
+
+    readInfo->name = symRead->name = "read";
+    readInfo->flag = 2;
+    readInfo->retType = 0;
+    readInfo->param = NULL;
+    symRead->info = readInfo;
+    symRead->type = 0;
+    symRead->left = symRead->right = NULL;
+    searchSymbol(symRead->name, 1, &insLoc);
+    addSymbol(symRead, insLoc);
+
+    writeInfo->name = symWrite->name = "write";
+    writeInfo->flag = 2;
+    writeInfo->retType = 0;
+    writeInfo->param = newVarList();
+    symWrite->info = writeInfo;
+    symWrite->type = 0;
+    symWrite->left = symWrite->right = NULL;
+    param = malloc(sizeof(varInfo));
+    param->type = param->isArray = 0;
+    param->arrInfo = NULL;
+    param->name = "num";
+    addVariable(writeInfo->param, param);
+
+    searchSymbol(symWrite->name, 1, &insLoc);
+    addSymbol(symWrite, insLoc);
 }
