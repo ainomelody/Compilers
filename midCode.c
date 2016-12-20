de75927c2703daa686dafe70c8f261308175bc39
ainomelody <qingqingdao5566@126.com>
2016-12-20 22:34:22 +0800

    typo

--- midCode.c
+++ midCode.c
@@ -71,7 +71,7 @@ void addCode(int op, int target, valueSt *arg1, valueSt *arg2)
 
     newCode->op = op;
     newCode->target = target;
-    if ((op == 3 || op == 5) && arg1->isImm) {
+    if ((op == 3 || op == 5) && arg1->isImm == 1) {
         newCode->arg1 = *arg2;
         newCode->arg2 = *arg1;
     } else {
